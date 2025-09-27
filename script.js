49228b5e53509815123fe1f755014e1e39566d67
Qaestio <118625380+Qaestio@users.noreply.github.com>
2025-09-26 20:34:45 -0400

    some shtuff

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("brother").innerHTML = "my lovely brother";
+
+alert("the world isn't ending");
+console.log("or is it");
+
+let name = "Jeff";
+let age = 120;
+
+//oldge
+function evaluateAge() {
+    return age > 100 ? "wow you're old" : "wow you're young";
+}
